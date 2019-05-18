af4a6f62128448f7c51d8cade930191203d23b0b
achlanbima <34331373+achlanbima@users.noreply.github.com>
2019-05-18 15:58:49 +0700

    Jawaban nomor 1 dari soal Arkademy batch 10

---
+++ 1.js
@@ -0,0 +1,37 @@
+function biodata(){
+	data = {
+      name : "Achlan Bima Nurachman",
+      address : "Cileunyi, Kab. Bandung, Jawa Barat",
+      hobbies : ['Playing games', 'Cycling'],
+      is_married : false,
+      school : { 
+        highschool: 'SMK Bakti Nusantara 666'
+      },
+      skills : [
+        { 
+          name: 'Javascript', 
+          score: 65 
+        },
+        { 
+          name: 'HTML', 
+          score: 70
+        },
+        { 
+          name: 'CSS', 
+          score: 60 
+        },
+        { 
+          name: 'PHP', 
+          score: 63 
+        },
+        { 
+          name: 'MYSQL', 
+          score: 65 
+        }
+      ]
+  };
+  
+  return JSON.stringify(data);
+}
+console.log(JSON.parse(biodata()));
+console.log(biodata());
